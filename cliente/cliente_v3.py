c7e59157287e4f780a0d10c5ba4f9eafdd9b82b0
gustavo <gusgard@gmail.com>
2013-12-17 18:16:16 -0200

    agrego multi threading para el cliente.

--- cliente/cliente_v3.py
+++ cliente/cliente_v3.py
@@ -3,6 +3,26 @@ import socket
 import sys
 from time import sleep
 from vehiculo import Vehiculo
+import threading
+import random
+
+class miThread(threading.Thread):
+    def __init__(self, ip, port, vehiculo, tiempo_muerto):
+        threading.Thread.__init__(self)
+        self.ip = ip
+        self.port = port
+        self.vehiculo = vehiculo
+        self.tiempo_muerto = tiempo_muerto
+
+    def run(self):
+        #print "Arranca" + str(self.vehiculo.id_vehiculo)
+        while 1:
+            sleep(self.tiempo_muerto)
+            generar_vehiculo(self.vehiculo)
+            client(self.ip, self.port, self.vehiculo.serializar())
+        #print "Finalizando... " + str(self.vehiculo.id_vehiculo)
+
+
 
 def client(ip, port, message):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -27,24 +47,23 @@ def generar_vehiculo(vehiculo):
     return vehiculo
 
 if __name__ == "__main__":
-    IP, PORT = "localhost", 9997
+    IP, PORT = "localhost", 9995
     #data = " ".join(sys.argv[1:])
-    id_vehiculo = sys.argv[1]
+    cantidad_vehiculos = int(sys.argv[1])
     #mensaje = sys.argv[2]
     #print "por parametro ", segundos, mensaje
 
-    cont = 0
-    vehiculo = Vehiculo(id_vehiculo,0,0,0)
+    #vehiculo = Vehiculo(id_vehiculo,0,0,0)
 
-    while 1:
-        sleep(0.1)
-        cont += 1
+    for cant in xrange(0, cantidad_vehiculos):
+        vehiculo = Vehiculo(cant, 0, 0, 0)
+        tiempo_muerto = float(random.randint(1, 100))/10
+        thread = miThread(IP, PORT, vehiculo, tiempo_muerto)
+        thread.start()
+
+    #while 1:
+    #    sleep(0.1)
         #client(IP, PORT, data+str(cont))
-        generar_vehiculo(vehiculo)
-        client(IP, PORT, vehiculo.serializar())
-
-    #time.sleep(int(segundos))
-    #
-    #
-    #client(IP, PORT, "termino , mandnado segundo mensaje")
+        #generar_vehiculo(vehiculo)
+        #client(IP, PORT, vehiculo.serializar())
 
